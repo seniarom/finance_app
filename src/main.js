4492a9aa6129602d56a1f18da1e2ec112f407bd2
Kseniia <seniarom@gmail.com>
2022-12-08 18:00:29 +0300

    final

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App.vue'
+import store from './store'
+import router from "@/router";
+import modal from "@/pluguins/ModalWindow.js";
+import context from "@/pluguins/ContextMenu.js";
+import vuetify from './pluguins/vuetify'
+
+Vue.config.productionTip = false;
+Vue.use(modal)
+Vue.use(context)
+
+new Vue({
+  render: function (h) { return h(App) },
+  store,
+  vuetify,
+  router
+}).$mount('#app')
